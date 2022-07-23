37534ea3868a57ed7859828dd7bdaaba2d943ba6
dimchik_st@list.ru <dimchik_st@list.ru>
2022-07-23 12:29:28 +0300

    Install Prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: "mysql://admin:randompassword@localhost:3306/nest_auth",
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
